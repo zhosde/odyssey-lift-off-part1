6632b77903792e7c082098d30664fbfbec362728
Raphael Terrier <R4ph-t@users.noreply.github.com>
2021-04-19 14:12:38 +0200

    adding server catch()

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,9 @@
-const { ApolloServer, MockList } = require('apollo-server');
+const {ApolloServer, MockList} = require('apollo-server');
 const typeDefs = require('./schema');
 
 const mocks = {
   Query: () => ({
-    tracksForHome: () => new MockList([6, 9]),
+    tracksForHome: () => new MockList([6, 9])
   }),
   Track: () => ({
     id: () => 'track_01',
@@ -12,25 +12,30 @@ const mocks = {
       return {
         name: 'Grumpy Cat',
         photo:
-          'https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg',
+          'https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg'
       };
     },
     thumbnail: () =>
       'https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg',
     length: () => 1210,
-    modulesCount: () => 6,
-  }),
+    modulesCount: () => 6
+  })
 };
 
 const server = new ApolloServer({
   typeDefs,
-  mocks,
+  mocks
 });
 
-server.listen().then(() => {
-  console.log(`
+server
+  .listen()
+  .then(() => {
+    console.log(`
     🚀  Server is running!
     🔉  Listening on port 4000
     📭  Query at https://studio.apollographql.com/dev
 `);
-});
+  })
+  .catch(err => {
+    console.error(err);
+  });
