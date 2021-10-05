0672ca93c696f1b7a21f57f08b7c19a09b89620e
S <ssimac@SdeiMac.fritz.box>
2021-10-05 18:31:55 +0200

    client setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
 import GlobalStyles from './styles';
 import Pages from './pages';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache()
+})
+
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <GlobalStyles />
     <Pages />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
